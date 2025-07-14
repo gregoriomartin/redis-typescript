85d41207019deeef297da24fbb35cf03b779af5a
Greg <gregorio.martin92@gmail.com>
2025-07-14 16:40:08 -0300

    ECHO command

--- app/main.ts
+++ app/main.ts
@@ -6,8 +6,18 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
     // Handle connection
-    connection.on("data", (data: Buffer) => {
-        connection.write(`+PONG\r\n`);
+    connection.on("data", (data: string) => {
+        let command: string = data.trim().split(/\s+/)[0];
+        switch (command) {
+            default:
+            case "PING":
+                connection.write(`+PONG\r\n`);
+                break;
+            case "ECHO":
+                let str: string = data.trim().split(/\s+/).slice(1).join(" ");
+                connection.write(`$${str.length}+${str}\r\n`);
+                break;
+        }
     });
 });
 
