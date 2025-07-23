53bec465d52954a54b61dc2830091c9e4e53a7d8
Greg <gregorio.martin92@gmail.com>
2025-07-22 22:37:08 -0300

    add support for setting a key with an expiry

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,20 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const map: Map<string, string> = new Map<string, string>();
+const map = new Map<string, {
+    value: string;
+    expiresAt?: number; // timestamp in ms
+}>();
+
+setInterval(() => {
+    const now = Date.now();
+    for (const [key, entry] of map) {
+        if (entry.expiresAt && now > entry.expiresAt) {
+            console.log(`Cleaning up expired key: ${key}`);
+            map.delete(key);
+        }
+    }
+}, 50);
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
@@ -25,18 +38,12 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
                 connection.write(`$${str.length}\r\n${str}\r\n`);
                 break;
             case "SET":
-                console.log("SET command received");
-                const key: string = args[4];
-                const value: string = args[6];
-                map.set(key, value);
-                console.log(`Setting key: ${key}, value: ${value}`);
-                // Here you would typically store the key-value pair in a database or in-memory store
-                connection.write(`+OK\r\n`);
+                handleSetCommand(args, connection);
                 break;
             case "GET":
                 console.log("GET command received");
                 const getKey: string = args[4];
-                const getValue: string | undefined = map.get(getKey);
+                const getValue: string | undefined = map.get(getKey)?.value;
                 if (getValue !== undefined) {
                     console.log(`Getting key: ${getKey}, value: ${getValue}`);
                     connection.write(`$${getValue.length}\r\n${getValue}\r\n`);
@@ -48,5 +55,32 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         }
     });
 });
+
+
+function handleSetCommand(args: string[], connection: net.Socket) {
+    console.log("SET command received");
+
+    const key: string = args[4];
+    const value: string = args[6];
+
+    let expiresAt: number | undefined = undefined;
+
+    // Parse optional arguments
+    for (let i = 8; i < args.length; i += 2) {
+        const option = args[i]?.toUpperCase();
+        if (!option) continue;
+
+        switch (option) {
+            case "PX":
+                expiresAt = Date.now() + parseInt(args[i + 2]);
+                break;
+        }
+    }
+
+    map.set(key, { value, expiresAt });
+    console.log(`Setting key: ${key}, value: ${value}, expiresAt: ${expiresAt}`);
+
+    connection.write(`+OK\r\n`);
+}
 
 server.listen(6379, "127.0.0.1");
